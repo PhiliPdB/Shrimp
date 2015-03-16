06fb48f73344d9af70251a427e6abd05be3b4c5e
PhiliPdB <phlpdbrn@gmail.com>
2015-03-16 17:17:04 +0100

    Add Source
    Begin van de source voor de Shrimp

---
+++ Sources/Shrimp.ino
@@ -0,0 +1,22 @@
+// Pins
+const int ledPin = 13;
+const int buttonPin = 2;
+const int thermometerPin = 0;
+
+// Variables
+int travelTime = 0; // Total travel time in s
+int logInterval = 0; // Hoe vaak willen we loggen?
+
+void setup() {
+  // Initialize pins
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(thermometerPin, INPUT);
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
