577ca4879ff3fc39204c72cf7c8b9ab4d1448881
PhiliPdB <phlpdbrn@gmail.com>
2015-03-18 14:16:58 +0100

    Update source of Shrimp
    Shrimp kan nu de temperatuur loggen naar de console

--- Sources/Shrimp/Shrimp.ino
+++ Sources/Shrimp/Shrimp.ino
@@ -1,22 +1,26 @@
 // Pins
 const int ledPin = 13;
 const int buttonPin = 2;
-const int thermometerPin = 0;
+const int thermometerPin = A0;
 
 // Variables
 int travelTime = 0; // Total travel time in s
 int logInterval = 0; // Hoe vaak willen we loggen?
+int temperature = 0;
 
 void setup() {
   // Initialize pins
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  pinMode(thermometerPin, INPUT);
   
-  
+  Serial.begin(9600);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  temperature = analogRead(thermometerPin);
+  Serial.println(temperature, DEC);
+  delay(1);
+  
   
 }
