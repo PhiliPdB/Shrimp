ca69a98d8b61a320b1be65e206b64e45a2882ad9
Raptor <joel.koelewijn19999@gmail.com>
2015-03-22 15:57:40 +0100

    Begin van Data visualiseren

--- Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.Designer.cs
+++ Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.Designer.cs
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.data_tb = new System.Windows.Forms.TextBox();
+            this.port_name_tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.value_pb = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(419, 86);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(136, 45);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(419, 137);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(136, 45);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(419, 188);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(136, 45);
+            this.save_btn.TabIndex = 2;
+            this.save_btn.Text = "Save Data";
+            this.save_btn.UseVisualStyleBackColor = true;
+            // 
+            // data_tb
+            // 
+            this.data_tb.Location = new System.Drawing.Point(36, 45);
+            this.data_tb.Multiline = true;
+            this.data_tb.Name = "data_tb";
+            this.data_tb.Size = new System.Drawing.Size(275, 252);
+            this.data_tb.TabIndex = 3;
+            // 
+            // port_name_tb
+            // 
+            this.port_name_tb.Location = new System.Drawing.Point(419, 31);
+            this.port_name_tb.Multiline = true;
+            this.port_name_tb.Name = "port_name_tb";
+            this.port_name_tb.Size = new System.Drawing.Size(136, 20);
+            this.port_name_tb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "-50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(322, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "port name";
+            // 
+            // value_pb
+            // 
+            this.value_pb.Location = new System.Drawing.Point(36, 344);
+            this.value_pb.Name = "value_pb";
+            this.value_pb.Size = new System.Drawing.Size(519, 23);
+            this.value_pb.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 435);
+            this.Controls.Add(this.value_pb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.port_name_tb);
+            this.Controls.Add(this.data_tb);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.TextBox data_tb;
+        private System.Windows.Forms.TextBox port_name_tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar value_pb;
     }
 }
 
--- Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.cs
+++ Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Shrimp_Reader
 {
     public partial class Form1 : Form
     {
+        private SerialPort myport;
+        private DateTime datetime;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.portname = port_name_tb.Text;
+            myport.Parity = Parity.None;
+            myport.DataBits = 8;
+            myport.StopBits = StopBits.One;
+            myport.DataReceived += myport_DataReceived;
+            try{
+                myport.Open();
+                data_tb.Text = "";
+            }
+                catch(Exception ex){
+                    MessageBox.Show(ex.Message,"Error");
+                }
+        }
+
+        void myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            datetime = DateTime.Now;
+
+            string time = datetime.Hour + ":"+datetime.Minute+":"+datetime.Second;
+            string in_data = myport.ReadLine();
+            data_tb.Text = time+"\t\t\t\t\tt"+in_data;
+
         }
     }
 }
