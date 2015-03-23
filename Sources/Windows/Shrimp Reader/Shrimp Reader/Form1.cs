b4514b0aeb939863a10e3248db3c5ae82a303d56
PhiliPdB <phlpdbrn@gmail.com>
2015-03-23 10:10:32 +0100

    Little bug fix

--- Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.cs
+++ Sources/Windows/Shrimp Reader/Shrimp Reader/Form1.cs
@@ -9,42 +9,35 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
-namespace Shrimp_Reader
-{
-    public partial class Form1 : Form
-    {
+namespace Shrimp_Reader {
+    public partial class Form1 : Form {
         private SerialPort myport;
         private DateTime datetime;
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Click(object sender, EventArgs e)
-        {
+        private void Form1_Click(object sender, EventArgs e) {
 
         }
 
-        private void start_btn_Click(object sender, EventArgs e)
-        {
+        private void start_btn_Click(object sender, EventArgs e) {
             myport = new SerialPort();
             myport.BaudRate = 9600;
-            myport.portname = port_name_tb.Text;
+            myport.PortName = port_name_tb.Text;
             myport.Parity = Parity.None;
             myport.DataBits = 8;
             myport.StopBits = StopBits.One;
             myport.DataReceived += myport_DataReceived;
-            try{
+            try {
                 myport.Open();
                 data_tb.Text = "";
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message,"Error");
             }
-                catch(Exception ex){
-                    MessageBox.Show(ex.Message,"Error");
-                }
         }
 
-        void myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
+        void myport_DataReceived(object sender, SerialDataReceivedEventArgs e) {
             datetime = DateTime.Now;
 
             string time = datetime.Hour + ":"+datetime.Minute+":"+datetime.Second;
